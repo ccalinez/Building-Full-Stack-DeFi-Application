f30f4993ea3e004c96127a76a8e84a87e74b29ca
Cristian Alinez <cristian.alinez@pjn.gov.ar>
2025-09-08 15:53:18 -0300

    reward

--- defi-apps/src/backend/contracts/TokenPair.sol
+++ defi-apps/src/backend/contracts/TokenPair.sol
@@ -135,7 +135,49 @@ contract TokenPair is ITokenPair, ERC20, ReentrancyGuard  {
         uint256 amountAOut,
         uint256 amountBOut,
         address to
-    ) external override {}
+    ) external override {
+         // Step 1: Pre-transfer verification
+        require(amountAOut > 0 || amountBOut > 0, "INVALID_OUTPUT_AMOUNT");
+        (uint256 _reserveA, uint256 _reserveB, ) = getReserves();
+        require(
+            amountAOut < _reserveA && amountBOut < _reserveB,
+            "INSUFFICIENT_RESERVE"
+        );
+        address _tokenA = tokenA;
+        address _tokenB = tokenB;
+        require(to != _tokenA && to != _tokenB, "INVALID_OUTPUT_ADDRESS");
+
+        // Step 2: Perform the transfer
+        if (amountAOut > 0) _safeTransfer(_tokenA, to, amountAOut);
+        if (amountBOut > 0) _safeTransfer(_tokenB, to, amountBOut);
+
+        // Step 3: Verify if the input amount is sufficient
+        uint256 balance0 = IERC20(_tokenA).balanceOf(address(this));
+        uint256 balance1 = IERC20(_tokenB).balanceOf(address(this));
+        uint256 amountAIn = balance0 > _reserveA - amountAOut
+            ? balance0 - (_reserveA - amountAOut)
+            : 0;
+        uint256 amountBIn = balance1 > _reserveB - amountBOut
+            ? balance1 - (_reserveB - amountBOut)
+            : 0;
+        require(amountAIn > 0 || amountBIn > 0, "INSUFFICIENT_INPUT_AMOUNT");
+
+        // Step 4: Verify if the balances are sufficient for rewards
+        {
+            // Scope for balance{0,1}Adjusted, avoids stack to deep error.
+            uint256 balance0Adjusted = balance0 * 1000 - amountAIn * 2;
+            uint256 balance1Adjusted = balance1 * 1000 - amountBIn * 2;
+            require(
+                balance0Adjusted * balance1Adjusted >=
+                    reserveA * reserveB * 1000**2,
+                "INSUFFICIENT_LIQUIDITY"
+            );
+        }
+
+        // Step 5: Update the reserves with token balances
+        _setReserves(balance0, balance1);
+        emit Swap(msg.sender, amountAIn, amountBIn, amountAOut, amountBOut, to);
+    }
 
     function skim(address to) external override {}
 
